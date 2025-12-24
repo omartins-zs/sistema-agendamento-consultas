c970b17aac10e1a9596dfaf05083166fb6b451ef
omartins-zs <gabrielmatheus209@gmail.com>
2025-12-24 12:28:27 -0300

    ✅ Atualizando teste de exemplo

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('the application redirects to agenda', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect(route('agenda.index'));
 });
