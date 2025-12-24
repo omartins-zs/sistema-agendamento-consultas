2979c3b97d06252150f8b3c67ad4ac90d6625777
omartins-zs <gabrielmatheus209@gmail.com>
2025-12-24 12:28:09 -0300

    🔧 Configurando rotas web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\ConsultaController;
+use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\SalaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('agenda.index');
 });
+
+Route::resource('medicos', MedicoController::class);
+Route::resource('pacientes', PacienteController::class);
+Route::resource('salas', SalaController::class);
+
+Route::resource('consultas', ConsultaController::class);
+Route::post('consultas/{consulta}/cancelar', [ConsultaController::class, 'cancelar'])->name('consultas.cancelar');
+Route::post('consultas/{consulta}/remarcar', [ConsultaController::class, 'remarcar'])->name('consultas.remarcar');
+
+Route::get('agenda', [AgendaController::class, 'index'])->name('agenda.index');
