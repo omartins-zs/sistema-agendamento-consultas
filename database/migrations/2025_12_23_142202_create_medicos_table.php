9922d943f3223efcd508193ffb23f29625e15810
omartins-zs <gabrielmatheus209@gmail.com>
2025-12-24 12:27:22 -0300

    🗃️ Criando migration de médicos

---
+++ database/migrations/2025_12_23_142202_create_medicos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('crm', 20)->unique();
+            $table->string('especialidade', 100);
+            $table->string('telefone', 20)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicos');
+    }
+};
