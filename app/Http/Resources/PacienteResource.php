1b4198540a2ef8480baf29c25e9c1bdd877c7c97
omartins-zs <gabrielmatheus209@gmail.com>
2025-12-24 12:27:55 -0300

    ✨ Criando Resource de Paciente

---
+++ app/Http/Resources/PacienteResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PacienteResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'cpf' => $this->cpf,
+            'data_nascimento' => $this->data_nascimento?->format('Y-m-d'),
+            'telefone' => $this->telefone,
+            'email' => $this->email,
+            'consultas' => $this->whenLoaded('consultas', fn () => ConsultaResource::collection($this->consultas)),
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
